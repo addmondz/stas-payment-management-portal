a0d694195d03c384c9f6aadb21d921c08d979b93
edmondlang <edmond_wuiming@hotmail.com>
2024-12-19 21:52:11 +0800

    vue 3 instalation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+/**
+ * Create a Vue application instance and register components.
+ */
+const app = createApp({});
+
+// Register components globally (if needed)
+app.component('example-component', ExampleComponent);
+
+// Mount the Vue application to the DOM
+app.mount('#app');
